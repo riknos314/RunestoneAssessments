d9a1d121d9a3ae6959b498da4d6c05fe693e0cc8
flysaiah <mayeis01@yahoo.com>
2015-06-17 10:17:33 -0500

    Added datafile--both JS and Sphinx work great

--- datafile/datafile.js
+++ datafile/datafile.js
@@ -1,48 +1,93 @@
-   /*
-  Created by Isaiah Mayerchak and Kirby Olson on 6/1/15
-  */
+/*==========================================
+=======     Master datafile.js      ========
+============================================
+===     This file contains the JS for    ===
+===   the Runestone Datafile component.  ===
+============================================
+===              Created by              ===
+===           Isaiah Mayerchak           ===
+===                6/8/15                ===
+==========================================*/
 
-
-//start with basic parent stuff
-function RunestoneBase() {
+function RunestoneBase () {  // Basic parent stuff
 
 }
 
-RunestoneBase.prototype.logBookEvent = function(info) {
-    console.log("logging event " + this.divid);
+RunestoneBase.prototype.logBookEvent = function (info) {
+  console.log('logging event ' + this.divid);
 };
 
-RunestoneBase.prototype.logRunEvent = function(info) {
-    console.log("running " + this.divid);
+RunestoneBase.prototype.logRunEvent = function (info) {
+  console.log('running ' + this.divid);
 };
 
-var dfList = {};  //Data File dictionary
+var dfList = {};  // Dictionary that contains all instances of Datafile objects
 
 DataFile.prototype = new RunestoneBase();
 
-//<pre> constructor
-function DataFile(opts) {
-	if (opts) {
-		this.init(opts);
-	}
+function DataFile (opts) {
+  if (opts) {
+    this.init(opts);
+  }
 }
 
-DataFile.prototype.init = function(opts) {
-    RunestoneBase.apply(this, arguments);
-    var orig = opts.orig;  //entire <pre> element
-    this.origElem = orig;
-    this.divid = orig.id;
-    this.dataEdit = false
-    if ($(this.origElem).data('edit') === true) {
-    	this.dataEdit = true
-    }
-    this.contentText = this.origElem.innerHTML;
+/*========================================
+== Initialize basic DataFile attributes ==
+========================================*/
+DataFile.prototype.init = function (opts) {
+  RunestoneBase.apply(this, arguments);
+  var orig = opts.orig;  // entire <pre> element that will be replaced by new HTML
+  this.origElem = orig;
+  this.divid = orig.id;
+  this.dataEdit = false;
+  if ($(this.origElem).data('edit') === true) {
+    this.dataEdit = true;
+  }
+  this.displayClass = 'block';   // Users can specify the non-edit component to be hidden--default is not hidden
+  if ($(this.origElem).is('[data-hidden]')) {
+    this.displayClass = 'none';
+  }
+  // Users can specify numbers of rows/columns when editing is true
+  this.numberOfRows = $(this.origElem).data('rows');
+  this.numberOfCols = $(this.origElem).data('cols');
 
+  if (this.dataEdit) {
+    this.createTextArea();
+  } else {
+    this.createPre();
+  }
+};
 
+/*=====================================
+== Create either <pre> or <textarea> ==
+==  depending on if editing is true  ==
+==================================*/
+DataFile.prototype.createPre = function () {   // If data edit is false
+  this.preContainer = document.createElement('pre');
+  this.preContainer.id = this.divid;
+  $(this.preContainer).attr({'style': 'display: ' + this.displayClass});
+  this.preContainer.innerHTML = this.origElem.innerHTML;
 
-$(document).ready(function() {
-    $('[data-component=datafile]').each( function(index ){
-        dfList[this.id] = new DataFile({'orig':this});
-    });
+  $(this.origElem).replaceWith(this.preContainer);
+};
+
+DataFile.prototype.createTextArea = function () {   // If data edit is true
+  this.textAreaContainer = document.createElement('textarea');
+  this.textAreaContainer.id = this.divid;
+  this.textAreaContainer.rows = this.numberOfRows;
+  this.textAreaContainer.cols = this.numberOfCols;
+  this.textAreaContainer.innerHTML = this.origElem.innerHTML;
+
+  $(this.origElem).replaceWith(this.textAreaContainer);
+};
+
+/*=================================
+== Find the custom HTML tags and ==
+==   execute our code on them    ==
+=================================*/
+$(document).ready(function () {
+  $('[data-component=datafile]').each(function (index) {
+    dfList[this.id] = new DataFile({'orig': this});
+  });
 
 });
