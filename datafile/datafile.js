c3319737c4c17750d6eaeba2258a9d997756ae0d
flysaiah <mayeis01@yahoo.com>
2015-06-25 16:39:32 -0500

    Reformatted codelens, DF, parsons, reveal, and TS w/new style

--- datafile/datafile.js
+++ datafile/datafile.js
@@ -9,76 +9,76 @@
 ===                6/8/15                ===
 ==========================================*/
 
-function RunestoneBase () {  // Basic parent stuff
+function RunestoneBase () {   // Basic parent stuff
 
 }
 
 RunestoneBase.prototype.logBookEvent = function (info) {
-  console.log('logging event ' + this.divid);
+    console.log("logging event " + this.divid);
 };
-
 RunestoneBase.prototype.logRunEvent = function (info) {
-  console.log('running ' + this.divid);
+    console.log("running " + this.divid);
 };
 
-var dfList = {};  // Dictionary that contains all instances of Datafile objects
+var dfList = {};    // Dictionary that contains all instances of Datafile objects
 
-DataFile.prototype = new RunestoneBase();
 
 function DataFile (opts) {
-  if (opts) {
-    this.init(opts);
-  }
+    if (opts) {
+        this.init(opts);
+    }
 }
+
+DataFile.prototype = new RunestoneBase();
 
 /*========================================
 == Initialize basic DataFile attributes ==
 ========================================*/
 DataFile.prototype.init = function (opts) {
-  RunestoneBase.apply(this, arguments);
-  var orig = opts.orig;  // entire <pre> element that will be replaced by new HTML
-  this.origElem = orig;
-  this.divid = orig.id;
-  this.dataEdit = false;
-  if ($(this.origElem).data('edit') === true) {
-    this.dataEdit = true;
-  }
-  this.displayClass = 'block';   // Users can specify the non-edit component to be hidden--default is not hidden
-  if ($(this.origElem).is('[data-hidden]')) {
-    this.displayClass = 'none';
-  }
-  // Users can specify numbers of rows/columns when editing is true
-  this.numberOfRows = $(this.origElem).data('rows');
-  this.numberOfCols = $(this.origElem).data('cols');
+    RunestoneBase.apply(this, arguments);
+    var orig = opts.orig;    // entire <pre> element that will be replaced by new HTML
+    this.origElem = orig;
+    this.divid = orig.id;
+    this.dataEdit = false;
+    if ($(this.origElem).data("edit") === true) {
+        this.dataEdit = true;
+    }
+    this.displayClass = "block";     // Users can specify the non-edit component to be hidden--default is not hidden
+    if ($(this.origElem).is("[data-hidden]")) {
+        this.displayClass = "none";
+    }
+    // Users can specify numbers of rows/columns when editing is true
+    this.numberOfRows = $(this.origElem).data("rows");
+    this.numberOfCols = $(this.origElem).data("cols");
 
-  if (this.dataEdit) {
-    this.createTextArea();
-  } else {
-    this.createPre();
-  }
+    if (this.dataEdit) {
+        this.createTextArea();
+    } else {
+        this.createPre();
+    }
 };
 
 /*=====================================
 == Create either <pre> or <textarea> ==
 ==  depending on if editing is true  ==
 ==================================*/
-DataFile.prototype.createPre = function () {   // If data edit is false
-  this.preContainer = document.createElement('pre');
-  this.preContainer.id = this.divid;
-  $(this.preContainer).attr({'style': 'display: ' + this.displayClass});
-  this.preContainer.innerHTML = this.origElem.innerHTML;
+DataFile.prototype.createPre = function () {     // If data edit is false
+    this.preContainer = document.createElement("pre");
+    this.preContainer.id = this.divid;
+    $(this.preContainer).attr({"style": "display: " + this.displayClass});
+    this.preContainer.innerHTML = this.origElem.innerHTML;
 
-  $(this.origElem).replaceWith(this.preContainer);
+    $(this.origElem).replaceWith(this.preContainer);
 };
 
-DataFile.prototype.createTextArea = function () {   // If data edit is true
-  this.textAreaContainer = document.createElement('textarea');
-  this.textAreaContainer.id = this.divid;
-  this.textAreaContainer.rows = this.numberOfRows;
-  this.textAreaContainer.cols = this.numberOfCols;
-  this.textAreaContainer.innerHTML = this.origElem.innerHTML;
+DataFile.prototype.createTextArea = function () {     // If data edit is true
+    this.textAreaContainer = document.createElement("textarea");
+    this.textAreaContainer.id = this.divid;
+    this.textAreaContainer.rows = this.numberOfRows;
+    this.textAreaContainer.cols = this.numberOfCols;
+    this.textAreaContainer.innerHTML = this.origElem.innerHTML;
 
-  $(this.origElem).replaceWith(this.textAreaContainer);
+    $(this.origElem).replaceWith(this.textAreaContainer);
 };
 
 /*=================================
@@ -86,8 +86,8 @@ DataFile.prototype.createTextArea = function () {   // If data edit is true
 ==   execute our code on them    ==
 =================================*/
 $(document).ready(function () {
-  $('[data-component=datafile]').each(function (index) {
-    dfList[this.id] = new DataFile({'orig': this});
-  });
+    $("[data-component=datafile]").each(function (index) {
+        dfList[this.id] = new DataFile({"orig": this});
+    });
 
 });
